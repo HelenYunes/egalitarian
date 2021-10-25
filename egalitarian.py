f514f2a70580f4f02207197c58694dacaf55cfce
HelenYunes <74502978+HelenYunes@users.noreply.github.com>
2021-10-26 02:15:27 +0300

    Update egalitarian.py

--- egalitarian.py
+++ egalitarian.py
@@ -1,38 +1,36 @@
 import cvxpy
 
 def egalitarian_division(matrix: list):
-        m = len(matrix[0])
-        n = len(matrix)
-        all_var, constraints = [[cvxpy.Variable() for _ in range(m)] for _ in range(n)], []
-        z = cvxpy.Variable()
+    m = len(matrix[0])
+    n = len(matrix)
+    all_var, constraints = [[cvxpy.Variable() for _ in range(m)] for _ in range(n)], []
+    z = cvxpy.Variable()
        
-        for i in range(n):
-            temp = 0
-            for j in range(m):
-                temp += matrix[i][j] *all_var[i][j] 
-                constraints.append(all_var[i][j] >= 0)
-            constraints.append(temp >= z)
-
+    for i in range(n):
+        temp = 0
         for j in range(m):
-            temp = 0
-            for i in range(n):
-                temp +=  all_var[i][j]
-            constraints.append(temp == 1)
+            temp += matrix[i][j] *all_var[i][j] 
+            constraints.append(all_var[i][j] >= 0)
+        constraints.append(temp >= z)
 
-        prob = cvxpy.Problem(cvxpy.Maximize(z), constraints)
-        prob.solve()
-
+    for j in range(m):
+        temp = 0
         for i in range(n):
-            print("Agent #{} ".format(i + 1), end='')
-            for j in range(m):
-                print(" gets {:.2f} of resource #{}".format(all_var[i][j].value, j + 1), end='')
-                print(',' if (j + 1) != m else '.\n', end='')
+            temp +=  all_var[i][j]
+        constraints.append(temp == 1)
+
+    prob = cvxpy.Problem(cvxpy.Maximize(z), constraints)
+    prob.solve()
+
+    for i in range(n):
+        print("Agent #{} ".format(i + 1), end='')
+        for j in range(m):
+            print(" gets {:.2f} of resource #{}".format(all_var[i][j].value, j + 1), end='')
+            print(',' if (j + 1) != m else '.\n', end='')
 
 def main():
-    mat = [
-        [81, 19, 1],
-        [70, 1, 29],
-    ]
+    mat = [[81, 19, 1],
+        [70, 1, 29],]
 
     egalitarian_division(mat)
 
